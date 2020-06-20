6f9c855908cc144fc1b9ec2f9fb4ed0b7c434b3c
CypressPointStrata <cypressnw2050@gmail.com>
2020-06-19 20:42:52 -0700

    update

--- update.bat
+++ update.bat
@@ -1,3 +1,9 @@
+REM To switch user on Windows:
+REM  Control Panel >> User Account >> Credential Manager >> Windows Credential >> Git Credential
+REM remove git credential.
+REM next time when you'll push repo it'll ask you for credential.
+git config --global user.name "CypressPointStrata"
+git config --global user.email "cypressnw2050@gmail.com"
 git add --all
 git commit -m "update"
 git push -u origin master

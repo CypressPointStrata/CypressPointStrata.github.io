289ecc0d57374ed7e97dc5f1c5b0362043a53513
CypressPointStrata <cypressnw2050@gmail.com>
2020-07-03 09:48:14 -0700

    update

--- updateGitWebsite.bat
+++ updateGitWebsite.bat
@@ -3,7 +3,7 @@ git config --global user.name CypressPointStrata
 git config --global user.email cypressnw2050@gmail.com
 REM YOU MIGHT HAVE TO WAIT A WHILE IF SWITCHING USERS
 REM Might have to do this too:
-REM Control Panel >> User Account >> Credential Manager >> Windows Credential >> Git Credential
+REM Control Panel >> User Accounts >> Manage your Credentials >> Windows Credential >> Git Credential
 REM or from cmd prompt: rundll32.exe keymgr.dll, KRShowKeyMgr
 REM remove git credentials.
 REM next time when you'll push repo it'll ask you for credential.
